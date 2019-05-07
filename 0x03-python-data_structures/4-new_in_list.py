34d0498e88179bfac6002d4411f4cd45b9a805ce
JohnCook17 <655@holbertonschool.com>
2019-05-07 21:38:59 +0000

    fixed an index issue

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     listcopy = my_list.copy()
     if idx < 0:
         return listcopy
-    if idx > len(my_list):
+    if idx > len(my_list) - 1:
         return listcopy
     listcopy[idx] = element
     return listcopy
