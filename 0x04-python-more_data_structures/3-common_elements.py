ae813d20f6308b033d28edf6f1b1d5237cc47840
JohnCook17 <655@holbertonschool.com>
2019-05-09 21:42:26 +0000

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-#    return [list(map(lambda x, y: x if x == y else None, set_1, set_2))]
     new_set = set()
     for i in set_1:
         for j in set_2:
