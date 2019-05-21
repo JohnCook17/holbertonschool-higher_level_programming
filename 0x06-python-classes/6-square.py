31c0af27689799cb735b9d0af0914b35549b7019
JohnCook17 <655@holbertonschool.com>
2019-05-21 22:57:16 +0000

    first commit of the day

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+class Square:
+
+    def __init__(self, size=0, position=(0,0)):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__position = position
+        if len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0] and position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+    def area(self):
+        return self.__size * self.__size
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    @property
+    def position(self):
+        return self.__position
+    @position.setter
+    def position(self, value):
+        if len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0] and position[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+    def my_print(self):
+        for i in range(self.__size):
+            print("#" * self.__size , end="")
+            print("")
