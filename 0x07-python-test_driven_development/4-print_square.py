ee5c093412b5a86509d777f593e9f8be2e934c15
JohnCook17 <655@holbertonschool.com>
2019-05-25 06:01:23 +0000

    update all files

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
+""" prints a square"""
+
+
 def print_square(size):
+    """prints a square
+
+    Args:
+        size (int): the size of the square to print
+
+    Raises:
+        TypeError
+        ValueError
+
+    """
+
     if type(size) is not int:
         raise TypeError("size must be an integer")
-    if size < 0:
+    if size <= 0:
         raise ValueError("size must be >= 0")
     if size == 0:
             print("")
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
+""" Text indentation adds two newlines after ".", "?", ":"""""
+
+
 def text_indentation(text):
+    """Text indentation
+
+    Args:
+        text (str): text to indent
+
+    Raise:
+        TypeError
+
+    """
+    text = text.strip()
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     prev =""
