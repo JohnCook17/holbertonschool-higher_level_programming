305ca4dd6c6207ec53432feabda5d8115986e3d6
root <root@DESKTOP-PELDMRR.localdomain>
2019-05-28 17:19:23 -0700

    4-7

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,64 @@
+class Rectangle:
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
+        self.__width = width
+        self.__height = height
+        if type(width)is not int:
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("Width must be >= 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+    
+    @property
+    def width(self):
+        return self.__width
+    
+    @property
+    def height(self):
+        return self.__height
+    
+    @width.setter
+    def width(self, value):
+        if type(value)is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("Width must be >= 0")
+        self.__width = value
+    
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    
+    def area(self):
+        return self.__width * self.__height
+    
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * self.__width + 2 * self.__height
+
+    def __str__(self):
+        h = 0
+        rect = ""
+        while h < self.__height:
+            rect += ("#" * self.__width)
+            rect += ("\n")
+            h += 1
+        return rect
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
