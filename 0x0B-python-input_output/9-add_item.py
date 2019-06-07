a55a21f0d3212da5c353c2f4cdf5286e7709e69b
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-07 13:11:52 -0700

    test git permissions

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -5,6 +5,7 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
+# foo
 if os.path.exists("add_item.json"):
     obj = load_from_json_file("add_item.json")
 else:
