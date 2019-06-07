ba5ad267107815b142f6614c86711469b1e21496
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-06 19:41:38 -0700

    removed bad code

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,14 +3,14 @@ import sys
 my_save = __import__("7-save_to_json_file")
 my_load = __import__("8-load_from_json_file")
 
+my_file = []
 try:
     my_file = my_load.load_from_json_file("add_item.json")
 except:
-    with open("add_item.json", mode="w") as my_file1:
-        my_file = []
+ """   with open("add_item.json", mode="w") as open_file:
         my_save.save_to_json_file(my_file, "add_item.json")
     my_file = my_load.load_from_json_file("add_item.json")
-
+"""
 if len(sys.argv) < 2:
     pass
 else:
