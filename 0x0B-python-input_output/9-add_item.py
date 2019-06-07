abbd3fa1dc35340196976b0d7b19e2306508ae64
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-06 20:05:46 -0700

    reformated

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,11 +3,11 @@ import sys
 my_save = __import__("7-save_to_json_file")
 my_load = __import__("8-load_from_json_file")
 
-obj = []
+
 try:
     obj = my_load.load_from_json_file("add_item.json")
 except:
-    pass
+    obj = []
 if not len(sys.argv) < 2:
     for arg in sys.argv[1:]:
         obj.append(arg)
