5ebaf4c9845c079917e8862a4fc13c60c84127fa
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-06 19:53:28 -0700

    reformated

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,17 +3,15 @@ import sys
 my_save = __import__("7-save_to_json_file")
 my_load = __import__("8-load_from_json_file")
 
-my_file = []
+obj = []
 try:
-    my_file = my_load.load_from_json_file("add_item.json")
+    obj = my_load.load_from_json_file("add_item.json")
 except:
  """   with open("add_item.json", mode="w") as open_file:
         my_save.save_to_json_file(my_file, "add_item.json")
     my_file = my_load.load_from_json_file("add_item.json")
 """
-if len(sys.argv) < 2:
-    pass
-else:
+if not len(sys.argv) < 2:
     for arg in sys.argv[1:]:
-        my_file.append(arg)
-my_save.save_to_json_file(my_file, "add_item.json")
+        obj.append(arg)
+my_save.save_to_json_file(obj, "add_item.json")
