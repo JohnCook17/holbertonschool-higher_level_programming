910787f3c9e4b877a6209eacec3e102ef46d53ad
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-06 20:56:39 -0700

    storing dict properly

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -2,16 +2,19 @@
 
 
 class Student:
-
+    
+    
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        ats = {}
         if attrs is None:
             return self.__dict__
         else:
             for key in self.__dict__:
                 if key in attrs:
-                    return (key, self.__dict__[key])
+                     ats[key] = self.__dict__[key]
+            return ats
