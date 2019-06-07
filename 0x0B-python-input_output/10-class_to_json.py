5884858cc46d22c17e95e395825b725b0987bc86
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-06 19:06:58 -0700

    pep8

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+
+
 def class_to_json(obj):
+
     return obj.__dict__
--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 class Student:
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
@@ -6,11 +8,12 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        if attrs == None:
+        if attrs is None:
             return self.__dict__
         else:
             for key in self.__dict__:
                 if key in attrs:
                     return (key, self.__dict__[key])
+
     def reload_from_json(self, json):
         return self.__dict__
