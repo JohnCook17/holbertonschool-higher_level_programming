0d1035c862b1026b8c3211be11c5aaf1a9114936
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-06 20:36:58 -0700

    changed import

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
@@ -8,4 +8,4 @@ try:
     obj = load_from_json_file("add_item.json")
 except:
     obj = []
-save_to_json_file(obj + sys.argv[1:], "add_item.json")
+save_to_json_file(obj + argv[1:], "add_item.json")
