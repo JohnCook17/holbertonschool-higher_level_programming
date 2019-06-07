0e520724237752c679f8497c495b1ec0880efc3a
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-07 11:00:27 -0700

    using the os path method

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import json
+import os
 from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
-try:
+if os.path.exists("add_item.json"):
     obj = load_from_json_file("add_item.json")
-except:
+else:
     obj = []
 save_to_json_file(obj + argv[1:], "add_item.json")
