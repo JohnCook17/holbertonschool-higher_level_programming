41f6364cc55ea3989b8a6b3e1db1608ec880ef65
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-06 19:55:34 -0700

    reformated

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -7,10 +7,7 @@ obj = []
 try:
     obj = my_load.load_from_json_file("add_item.json")
 except:
- """   with open("add_item.json", mode="w") as open_file:
-        my_save.save_to_json_file(my_file, "add_item.json")
-    my_file = my_load.load_from_json_file("add_item.json")
-"""
+    pass
 if not len(sys.argv) < 2:
     for arg in sys.argv[1:]:
         obj.append(arg)
