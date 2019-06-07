d535d82ad871338c8a7931eb8b1ca5edfb286a29
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-06 21:01:39 -0700

    changed 12 so had to change 13

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -2,18 +2,21 @@
 
 
 class Student:
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        ats = {}
         if attrs is None:
             return self.__dict__
         else:
             for key in self.__dict__:
                 if key in attrs:
-                    return (key, self.__dict__[key])
+                    ats[key] = self.__dict__[key]
+            return ats
 
     def reload_from_json(self, json):
         return self.__dict__
