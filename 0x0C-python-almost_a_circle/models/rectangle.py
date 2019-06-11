ac577afbb965b818fb932fd672de9ee975cf8be2
root <root@DESKTOP-PELDMRR.localdomain>
2019-06-10 21:43:29 -0700

    10-13

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -114,3 +114,12 @@ class Rectangle(Base):
                     self.x = kwargs["x"]
                 if key == "y":
                     self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        my_dict = {}
+        my_dict["id"] = self.id
+        my_dict["width"] = self.width
+        my_dict["height"] = self.height
+        my_dict["x"] = self.x
+        my_dict["y"] = self.y
+        return my_dict
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,6 +5,44 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(width=size, height=size, x=x, y=y, id=id)
-
+        
     def __str__(self):
-        return "[Square] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            for key in kwargs:
+                if key == "id":
+                   self.id = kwargs["id"]
+                if key == "size":
+                    self.size = kwargs["size"]
+                if key == "x":
+                    self.x = kwargs["x"]
+                if key == "y":
+                    self.y = kwargs["y"]
+
+    def to_dictionary(self):
+        my_dict = {}
+        my_dict["id"] = self.id
+        my_dict["size"] = self.size
+        my_dict["x"] = self.x
+        my_dict["y"] = self.y
+        return my_dict
