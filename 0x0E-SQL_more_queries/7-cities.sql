9549ff6699d1516343b4019a7593a978ec25697a
JohnCook17 <655@holbertonschool.com>
2019-07-16 12:52:43 -0700

    added comments

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,9 @@
 -- Creates a db if not exist and a table in the db
 -- Uses a foreign key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use
 USE hbtn_0d_usa;
+-- creates table
 CREATE TABLE IF NOT EXISTS cities
 (id INT UNIQUE AUTO_INCREMENT NOT NULL, 
 name VARCHAR(256) NOT NULL, 
