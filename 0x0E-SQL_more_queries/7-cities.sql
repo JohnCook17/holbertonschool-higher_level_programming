4eca47cd8a54ba795dd271397f6ffc9ffa05c135
JohnCook17 <655@holbertonschool.com>
2019-07-16 12:33:51 -0700

    use instead of dot

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,4 +1,5 @@
 -- Creates a db if not exist and a table in the db
 -- Uses a foreign key
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (`id` INT NOT NULL AUTO_INCREMENT UNIQUE, `name` VARCHAR(256) NOT NULL, `state_id` INT NOT NULL, PRIMARY KEY (`id`), FOREIGN KEY (`state_id`) REFERENCES states (`id`));
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (`id` INT NOT NULL AUTO_INCREMENT UNIQUE, `name` VARCHAR(256) NOT NULL, `state_id` INT NOT NULL, PRIMARY KEY (`id`), FOREIGN KEY (`state_id`) REFERENCES states (`id`));
