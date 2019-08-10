e9d5e6580926fd2d937630c81c9f365c1aa1120b
JohnCook17 <655@holbertonschool.com>
2019-08-10 20:01:12 +0000

    used binary

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Write a script that lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa:
+"""
 
 if __name__ == "__main__":
     import sys
@@ -10,7 +13,7 @@ if __name__ == "__main__":
                                passwd=sys.argv[2],
                                db=sys.argv[3])
     cur = database.cursor()
-    cur.execute("SELECT states.id, states.name FROM states WHERE name like 'N%' ORDER BY states.id ASC")
+    cur.execute("SELECT states.id, states.name FROM states WHERE name like BINARY 'N%' ORDER BY states.id ASC")
     for row in cur.fetchall():
         print (row)
     cur.close()
