685a2855c10ed9ea79331c0d975ca2f0ec611b52
JohnCook17 <655@holbertonschool.com>
2019-08-10 20:45:32 +0000

    utf8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
 
     database = MySQLdb.connect(host="localhost",
                                port=3306,
+                               charset="utf8",
                                user=argv[1],
                                passwd=argv[2],
                                db=argv[3])
