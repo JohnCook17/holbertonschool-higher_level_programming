4209f554ed93ff943b0cb755d36c3f2bff194d93
JohnCook17 <655@holbertonschool.com>
2019-08-10 19:10:24 +0000

    made the order by more explicit

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                                db=sys.argv[3])
     cur = database.cursor()
     cur.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.state_id = states.id ORDER BY id ASC")
+        "SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
     for row in cur.fetchall():
         print (row)
     database.close()
