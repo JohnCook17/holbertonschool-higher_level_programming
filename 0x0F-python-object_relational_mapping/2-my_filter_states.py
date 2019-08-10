7f2eee640a86ef50210a695d5d96fb81eb66c793
JohnCook17 <655@holbertonschool.com>
2019-08-10 20:05:37 +0000

    added binary, comments and cur.close

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""Write a script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa where name
+matches the argument.
+"""
 
 if __name__ == "__main__":
     import sys
@@ -12,8 +15,9 @@ if __name__ == "__main__":
                                db=sys.argv[3])
     cur = database.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE name like '{}' ORDER BY id ASC"
+        "SELECT * FROM states WHERE name like BINARY '{}' ORDER BY id ASC"
         .format(sys.argv[4]))
     for row in cur.fetchall():
         print (row)
+    cur.close()
     database.close()
