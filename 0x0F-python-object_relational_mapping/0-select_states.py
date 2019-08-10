51c569a22dfed8aad24e47e920a90ddf346cbeb4
JohnCook17 <655@holbertonschool.com>
2019-08-10 19:22:35 +0000

    states.id instead of id

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                                passwd=sys.argv[2],
                                db=sys.argv[3])
     cur = database.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     for row in cur.fetchall():
         print (row)
     database.close()
