3b9c572b70e3a15b01ca9127a419d866c66efaa8
JohnCook17 <655@holbertonschool.com>
2019-08-10 19:51:16 +0000

    made id states.id

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                                passwd=sys.argv[2],
                                db=sys.argv[3])
     cur = database.cursor()
-    cur.execute("SELECT states.id, states.name FROM states WHERE name like 'N%' ORDER BY id ASC")
+    cur.execute("SELECT states.id, states.name FROM states WHERE name like 'N%' ORDER BY states.id ASC")
     for row in cur.fetchall():
         print (row)
     database.close()
