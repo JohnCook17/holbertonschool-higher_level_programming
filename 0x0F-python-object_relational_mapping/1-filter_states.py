ab781a60b3f5d255820a9eb08eceddf3e2de1e57
JohnCook17 <655@holbertonschool.com>
2019-08-10 01:18:47 +0000

    1-3

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=sys.argv[1],
+                               passwd=sys.argv[2],
+                               db=sys.argv[3])
+    cur = database.cursor()
+    cur.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY id ASC")
+    for row in cur.fetchall():
+        print (row)
+    database.close()
---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=sys.argv[1],
+                               passwd=sys.argv[2],
+                               db=sys.argv[3])
+    cur = database.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name like '{}' ORDER BY id ASC"
+        .format(sys.argv[4]))
+    for row in cur.fetchall():
+        print (row)
+    database.close()
