a07b297791f6bc6b4e9c508e16c6c6cd39cc8fb8
JohnCook17 <655@holbertonschool.com>
2019-08-10 20:23:42 +0000

    documentation, cur.close()

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""Write a script that lists all cities from the database hbtn_0e_4_usa
+"""
 
 if __name__ == "__main__":
     import sys
@@ -12,8 +13,11 @@ if __name__ == "__main__":
                                db=sys.argv[3])
     cur = database.cursor()
     cur.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+        "SELECT cities.id, cities.name, states.name\
+        FROM cities LEFT JOIN states\
+        ON cities.state_id = states.id\
+        ORDER BY cities.id ASC")
     for row in cur.fetchall():
         print (row)
+    cur.close()
     database.close()
-
