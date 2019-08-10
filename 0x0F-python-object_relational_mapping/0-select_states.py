acf63c2853f5a8833904a5a7b3882409b4fd1b9e
JohnCook17 <655@holbertonschool.com>
2019-08-10 19:00:05 +0000

    restructure

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-import sys
-import MySQLdb
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
 
-
-database = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=sys.argv[1],
-                     passwd=sys.argv[2],
-                     db=sys.argv[3])
-cur = database.cursor()
-cur.execute("SELECT * FROM states")
-for row in cur.fetchall():
-    print (row)
-database.close()
+    database = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=sys.argv[1],
+                               passwd=sys.argv[2],
+                               db=sys.argv[3])
+    cur = database.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cur.fetchall():
+        print (row)
+    database.close()
