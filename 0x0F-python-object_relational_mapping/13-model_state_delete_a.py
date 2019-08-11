c88fcae8c27ec0af8f7ba23b773773a11e920323
JohnCook17 <655@holbertonschool.com>
2019-08-11 21:43:54 +0000

    13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Finds states with the letter a and deletes
+"""
+import MySQLdb
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    my_query = session.query(State).filter(State.name.like("%" + "a" + "%"))
+    my_query.delete('fetch')
+    session.commit()
+    session.close()
