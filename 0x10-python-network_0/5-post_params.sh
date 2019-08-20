e76b0ff228d128e02c922a07d9700d10a317077d
JohnCook17 <655@holbertonschool.com>
2019-08-20 13:54:56 -0700

    realized that we where not actualy trying to send an email

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # email
-echo 'I will always be here for PLD' > tmp.txt | curl -s --url $1 --mail-from 'hr@holbertonschool.com' --mail-rcpt 'hr@holbertonschool.com' --upload-file tmp.txt
+curl -s -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -X POST $1
