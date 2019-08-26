d6695d192843c763fd303f270bbe27ed54966df9
JohnCook17 <655@holbertonschool.com>
2019-08-26 12:55:01 -0700

    was using type on the wrong var

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+import urllib.request
 if __name__ == "__main__":
-    import urllib.request
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as data:
         print_data = data.read()
         print("Body response:")
-        print("\t- type: {}".format(type(data)))
+        print("\t- type: {}".format(type(print_data)))
         print("\t- content: {}".format(print_data))
         print("\t- utf8 content: {}".format(print_data.decode('utf-8')))
