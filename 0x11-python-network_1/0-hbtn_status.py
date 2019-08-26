223719c3c9030581b7138b90291f8f01d9c63bed
JohnCook17 <655@holbertonschool.com>
2019-08-26 12:06:34 -0700

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from urllib import request
+    with request.urlopen("https://intranet.hbtn.io/status") as data:
+        print_data = data.read()
+        print("Body response:")
+        print("\t- type: {}".format(data))
+        print("\t- content: {}".format(print_data))
+        print("\t- utf8 content: {}".format(print_data.decode('utf8')))
