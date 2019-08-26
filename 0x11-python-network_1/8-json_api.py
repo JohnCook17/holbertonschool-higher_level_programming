9566f0081ed476341a93e47a8e3fcd30d465aeb7
JohnCook17 <655@holbertonschool.com>
2019-08-26 16:56:22 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) != 2:
+        letter = ""
+    else:
+        letter = {"q": argv[1]}
+    my_data = requests.post("http://0.0.0.0:5000/search_user", letter)
+    if len(my_data.text) < 1:
+        print("No result")
+    try:
+        my_data.json()
+        print("[{}] {}".format(my_data.json()["id"], my_data.json()["name"]))
+    except:
+        print("Not a valid JSON")
