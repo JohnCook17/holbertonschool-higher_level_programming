b3b364ec09cc78a57445e4af4583cf9ae5a25403
JohnCook17 <655@holbertonschool.com>
2019-08-26 13:56:33 -0700

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+from urllib import request, parse
+from sys import argv
+if __name__ == "__main__":
+    data = parse.urlencode({"email": argv[2]}).encode()
+    req = request.Request(argv[1], data=data)
+    with request.urlopen(req) as result:
+        print(result.read().decode("utf-8"))
