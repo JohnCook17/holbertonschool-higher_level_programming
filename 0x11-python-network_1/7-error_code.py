f86ed71163c2a5605a94857d67f17d5cbc9ac70d
JohnCook17 <655@holbertonschool.com>
2019-08-26 15:51:03 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    site = requests.get(argv[1])
+    if site.status_code >= 400:
+        print("Error code: {}".format(site.status_code))
+    else:
+        print(site.text)
