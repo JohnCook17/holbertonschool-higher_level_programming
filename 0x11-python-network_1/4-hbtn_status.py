3da3875463ed7e0bb2d41e1d51a70fb8369a58e2
JohnCook17 <655@holbertonschool.com>
2019-08-26 21:50:52 +0000

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+import requests
+
+if __name__ == "__main__":
+    data = requests.get("https://intranet.hbtn.io/status")
+    print_data = data.text
+    print("Body response:")
+    print("\t- type: {}".format(type(print_data)))
+    print("\t- content: {}".format(print_data))
