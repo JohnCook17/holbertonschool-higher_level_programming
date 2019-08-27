3e037427b12545dd63713391577de4f301bf9e4f
JohnCook17 <655@holbertonschool.com>
2019-08-26 18:19:24 -0700

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    my_data = requests.get("https://api.github.com/user",
+                           auth=(argv[1], argv[2]))
+    print(my_data.json()["id"])
