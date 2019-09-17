e8102a0e2b86c2a7faa99782528ac97f8e58b567
JohnCook17 <655@holbertonschool.com>
2019-09-17 16:28:45 -0700

    fixed NaN handeling

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,4 +4,8 @@ const myInt = parseInt(arg1);
 function factorial (n) {
   return (n !== 1) ? n * factorial(n - 1) : 1;
 }
-console.log(factorial(myInt));
+if (isNaN(myInt)) {
+  console.log('1');
+} else {
+  console.log(factorial(myInt));
+}
