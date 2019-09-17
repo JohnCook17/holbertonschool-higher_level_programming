14ba2bb41eabc2526d7f6a84ec340d9bd0146eaa
JohnCook17 <655@holbertonschool.com>
2019-09-17 15:49:16 -0700

    7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const arg1 = process.argv[2];
+const myInt = parseInt(arg1);
+if (isNaN(myInt)) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  if (myInt > 0) {
+    while (i < myInt) {
+      console.log('C is fun');
+      i++;
+    }
+  }
+}
