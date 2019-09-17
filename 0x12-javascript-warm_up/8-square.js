8c32597686a4fb772b8fc8effe9e6bd1ad40230a
JohnCook17 <655@holbertonschool.com>
2019-09-17 15:57:17 -0700

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const arg1 = process.argv[2];
+const myInt = parseInt(arg1);
+const square = 'X';
+if (isNaN(myInt)) {
+  console.log('Missing size');
+} else {
+  let i = 0;
+  if (myInt > 0) {
+    while (i < myInt) {
+      console.log(square.repeat(myInt));
+      i++;
+    }
+  }
+}
