8c933928b97b61f336fed016fa62534ca6ec81de
JohnCook17 <655@holbertonschool.com>
2019-09-17 15:35:27 -0700

    semistandard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,7 +3,6 @@ const c = 'C is fun';
 const python = 'Python is cool';
 const java = 'Javascript is amazing';
 const array = [c, python, java];
-for (let i in array)
-{
-    console.log(array[i]);
+for (const i in array) {
+  console.log(array[i]);
 }
