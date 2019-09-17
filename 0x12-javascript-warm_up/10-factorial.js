fe91270729eddbf21c2beee076742cd75052224d
JohnCook17 <655@holbertonschool.com>
2019-09-17 16:24:17 -0700

    casting to int

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const arg1 = process.argv[2];
+const myInt = parseInt(arg1);
 function factorial (n) {
   return (n !== 1) ? n * factorial(n - 1) : 1;
 }
-console.log(factorial(arg1));
+console.log(factorial(myInt));
