7daee434e8aedbc6d2373bf7d7188aeb15828dd7
JohnCook17 <655@holbertonschool.com>
2019-09-17 15:25:26 -0700

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const arg1 = process.argv[2];
+const myInt = parseInt(arg1);
+if (isNaN(myInt)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + myInt);
+}
