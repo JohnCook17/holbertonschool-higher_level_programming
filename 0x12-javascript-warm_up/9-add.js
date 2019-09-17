8822a4fe697e4f5c04c63b65859b2f45e561421e
JohnCook17 <655@holbertonschool.com>
2019-09-17 16:07:00 -0700

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+const myInt = parseInt(arg1);
+const myInt2 = parseInt(arg2);
+const myInt3 = myInt + myInt2;
+console.log(myInt3);
