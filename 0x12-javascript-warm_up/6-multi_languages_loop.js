25c930b64af1168a07077586cc97f60283c8d5b4
JohnCook17 <655@holbertonschool.com>
2019-09-17 15:33:17 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const c = 'C is fun';
+const python = 'Python is cool';
+const java = 'Javascript is amazing';
+const array = [c, python, java];
+for (let i in array)
+{
+    console.log(array[i]);
+}
