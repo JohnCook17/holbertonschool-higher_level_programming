0ccdcff1af5dc78f039d81e7963b3bd2b7b1d67e
JohnCook17 <655@holbertonschool.com>
2019-09-17 17:10:52 -0700

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
+if (args <= 1) {
+  console.log('0');
+} else {
+  const myArray = [];
+  for (const i in args) {
+    myArray.push(parseInt(args[i]));
+  }
+  const myIndex = myArray.indexOf(Math.max.apply(Math, myArray));
+  if (myIndex > -1) {
+    myArray.splice(myIndex, 1);
+  }
+  console.log(Math.max.apply(Math, myArray));
+}
