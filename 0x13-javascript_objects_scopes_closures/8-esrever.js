e7f8d8a6546a329c55a5ac1a6321065000e1298b
JohnCook17 <655@holbertonschool.com>
2019-09-23 15:24:10 -0700

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+exports.esrever = function (list) {
+  let i = list.length;
+  const myList = [];
+  while (i > 0) {
+    myList.push(list[i - 1]);
+    i--;
+  }
+  return myList;
+};
