43a1be0ce9f8900d95ba36a40e78951c0ae74b26
JohnCook17 <655@holbertonschool.com>
2019-09-23 15:10:03 -0700

    7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let i = 0;
+  let results = 0;
+  for (i in list) {
+    if (list[i] === searchElement) {
+      results++;
+    }
+    i++;
+  }
+  return results;
+};
