de0035af5db4a3bda954fc40623075ef9cce938c
JohnCook17 <655@holbertonschool.com>
2019-09-23 14:59:24 -0700

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+
+const Square5 = require('./5-square');
+
+module.exports = class Square extends Square5 {
+  charPrint (c) {
+    if (c === undefined) {
+      super.print();
+    } else {
+      let i = 0;
+      while (i < this.height) {
+        console.log(c.repeat(this.height));
+        i++;
+      }
+    }
+  }
+};
